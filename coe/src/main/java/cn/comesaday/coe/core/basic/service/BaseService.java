7c2fae8ba4201c1c4e0bd51796122c37017363c1
chenwei12 <chenwei12@tuniu.com>
2021-01-12 15:54:10 +0800

    V202101121454 底层代码升级

---
+++ coe/src/main/java/cn/comesaday/coe/core/basic/service/BaseService.java
@@ -0,0 +1,149 @@
+package cn.comesaday.coe.core.basic.service;
+
+import cn.comesaday.coe.core.jpa.bean.repository.MyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * <描述> BaseService
+ * <详细背景>
+ *
+ * @author: ChenWei
+ * @CreateAt: 2021-01-12 15:40
+ */
+public abstract class BaseService<T, ID extends Serializable> implements MyRepository<T, ID> {
+
+    private MyRepository<T, ID> myRepository;
+
+    @Autowired
+    public MyRepository<T, ID> getMyRepository() {
+        return myRepository;
+    }
+
+    @Override
+    public <S extends T> S save(S s) {
+        return myRepository.save(s);
+    }
+
+    @Override
+    public void delete(T t) {
+        myRepository.delete(t);
+    }
+
+    @Override
+    public <S extends T> List<S> saveAll(Iterable<S> iterable) {
+        return myRepository.saveAll(iterable);
+    }
+
+    @Override
+    public List<T> findAll() {
+        return myRepository.findAll();
+    }
+
+    @Override
+    public List<T> findAll(Sort sort) {
+        return myRepository.findAll(sort);
+    }
+
+    @Override
+    public List<T> findAllById(Iterable<ID> iterable) {
+        return myRepository.findAllById(iterable);
+    }
+
+    @Override
+    public void flush() {
+        myRepository.flush();
+    }
+
+    @Override
+    public <S extends T> S saveAndFlush(S s) {
+        return myRepository.saveAndFlush(s);
+    }
+
+    @Override
+    public void deleteInBatch(Iterable<T> iterable) {
+        myRepository.deleteInBatch(iterable);
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+        myRepository.deleteAllInBatch();
+    }
+
+    @Override
+    public T getOne(ID id) {
+        return myRepository.getOne(id);
+    }
+
+    @Override
+    public <S extends T> List<S> findAll(Example<S> example) {
+        return myRepository.findAll(example);
+    }
+
+    @Override
+    public <S extends T> List<S> findAll(Example<S> example, Sort sort) {
+        return myRepository.findAll(example, sort);
+    }
+
+    @Override
+    public Page<T> findAll(Pageable pageable) {
+        return myRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return myRepository.findById(id);
+    }
+
+    @Override
+    public boolean existsById(ID id) {
+        return myRepository.existsById(id);
+    }
+
+    @Override
+    public long count() {
+        return myRepository.count();
+    }
+
+    @Override
+    public void deleteById(ID id) {
+        myRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends T> iterable) {
+        myRepository.deleteAll(iterable);
+    }
+
+    @Override
+    public void deleteAll() {
+        myRepository.deleteAll();
+    }
+
+    @Override
+    public <S extends T> Optional<S> findOne(Example<S> example) {
+        return myRepository.findOne(example);
+    }
+
+    @Override
+    public <S extends T> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return myRepository.findAll(example, pageable);
+    }
+
+    @Override
+    public <S extends T> long count(Example<S> example) {
+        return myRepository.count(example);
+    }
+
+    @Override
+    public <S extends T> boolean exists(Example<S> example) {
+        return myRepository.exists(example);
+    }
+}
