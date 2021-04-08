f12989cd4ce3cce327043fcf942a2e5b7bb2a31e
chenwei12 <chenwei12@tuniu.com>
2021-04-08 15:25:22 +0800

    V2021004081524 扩展整型、长整型常量

--- coe/src/main/java/cn/comesaday/coe/common/constant/NumConstant.java
+++ coe/src/main/java/cn/comesaday/coe/common/constant/NumConstant.java
@@ -9,31 +9,79 @@ package cn.comesaday.coe.common.constant;
  */
 public class NumConstant {
 
-    public final static int I01 = -1;
+    public static final Integer I01 = -1;
+    public static final Integer I0 = 0;
+    public static final Integer I1 = 1;
+    public static final Integer I2 = 2;
+    public static final Integer I3 = 3;
+    public static final Integer I4 = 4;
+    public static final Integer I5 = 5;
+    public static final Integer I6 = 6;
+    public static final Integer I7 = 7;
+    public static final Integer I8 = 8;
+    public static final Integer I9 = 9;
+    public static final Integer I10 = 10;
+    public static final Integer I12 = 12;
+    public static final Integer I16 = 16;
+    public static final Integer I19 = 19;
+    public static final Integer I20 = 20;
+    public static final Integer I23 = 23;
+    public static final Integer I24 = 24;
+    public static final Integer I29 = 29;
+    public static final Integer I30 = 30;
+    public static final Integer I32 = 32;
+    public static final Integer I59 = 59;
+    public static final Integer I60 = 60;
+    public static final Integer I99 = 99;
+    public static final Integer I100 = 100;
+    public static final Integer I200 = 200;
+    public static final Integer I201 = 201;
+    public static final Integer I303 = 303;
+    public static final Integer I400 = 400;
+    public static final Integer I403 = 403;
+    public static final Integer I404 = 404;
+    public static final Integer I500 = 500;
+    public static final Integer I999 = 999;
+    public static final Integer I1000 = 1000;
+    public static final Integer I1024 = 1024;
+    public static final Integer I5000 = 5000;
+    public static final Integer I60000 = 60000;
 
-    public final static int I0 = 0;
-
-    public final static int I1 = 1;
-
-    public final static int I2 = 2;
-
-    public final static int I3 = 3;
-
-    public final static int I4 = 4;
-
-    public final static int I5 = 5;
-
-    public final static int I6 = 6;
-
-    public final static int I7 = 7;
-
-    public final static int I8 = 8;
-
-    public final static int I9 = 9;
-
-    public final static int I10 = 10;
-
-    public final static int I11 = 11;
-
-    public final static int I12 = 12;
+    public static final Long L01 = -1L;
+    public static final Long L0 = 0L;
+    public static final Long L1 = 1L;
+    public static final Long L2 = 2L;
+    public static final Long L3 = 3L;
+    public static final Long L4 = 4L;
+    public static final Long L5 = 5L;
+    public static final Long L6 = 6L;
+    public static final Long L7 = 7L;
+    public static final Long L8 = 8L;
+    public static final Long L9 = 9L;
+    public static final Long L10 = 10L;
+    public static final Long L12 = 12L;
+    public static final Long L16 = 16L;
+    public static final Long L19 = 19L;
+    public static final Long L20 = 20L;
+    public static final Long L23 = 23L;
+    public static final Long L24 = 24L;
+    public static final Long L29 = 29L;
+    public static final Long L30 = 30L;
+    public static final Long L32 = 32L;
+    public static final Long L59 = 59L;
+    public static final Long L60 = 60L;
+    public static final Long L99 = 99L;
+    public static final Long L100 = 100L;
+    public static final Long L200 = 200L;
+    public static final Long L201 = 201L;
+    public static final Long L303 = 303L;
+    public static final Long L400 = 400L;
+    public static final Long L403 = 403L;
+    public static final Long L404 = 404L;
+    public static final Long L500 = 500L;
+    public static final Long L999 = 999L;
+    public static final Long L1000 = 1000L;
+    public static final Long L1024 = 1024L;
+    public static final Long L5000 = 5000L;
+    public static final Long L60000 = 60000L;
 }
