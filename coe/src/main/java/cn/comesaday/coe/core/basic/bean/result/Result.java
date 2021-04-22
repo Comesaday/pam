5f21d01afbcd8513c68db817e83e616b245ff52b
comesaday <931716599@qq.com>
2021-04-22 22:04:14 +0800

    V202104222154 底层返回json数据格式封装

--- coe/src/main/java/cn/comesaday/coe/core/basic/bean/result/Result.java
+++ coe/src/main/java/cn/comesaday/coe/core/basic/bean/result/Result.java
@@ -1,7 +1,5 @@
 package cn.comesaday.coe.core.basic.bean.result;
 
-import java.io.Serializable;
-
 /**
  * <Description> Result
  * @author ChenWei
@@ -43,61 +41,5 @@ public class Result {
 
     public static JsonResult fail(Object data) {
         return new JsonResult(Boolean.FALSE, data);
-    }
-
-    private static class JsonResult implements Serializable {
-
-        private boolean success = true;
-
-        private String message;
-
-        private Object data;
-
-        public JsonResult() {
-        }
-
-        public JsonResult(boolean success) {
-            this.success = success;
-        }
-
-        public JsonResult(boolean success, String message) {
-            this.success = success;
-            this.message = message;
-        }
-
-        public JsonResult(boolean success, Object data) {
-            this.success = success;
-            this.data = data;
-        }
-
-        public JsonResult(boolean success, String message, Object data) {
-            this.success = success;
-            this.message = message;
-            this.data = data;
-        }
-
-        public boolean isSuccess() {
-            return success;
-        }
-
-        public void setSuccess(boolean success) {
-            this.success = success;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public Object getData() {
-            return data;
-        }
-
-        public void setData(Object data) {
-            this.data = data;
-        }
     }
 }
